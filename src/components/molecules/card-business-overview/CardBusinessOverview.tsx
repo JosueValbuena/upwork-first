5449f085029f8bea0e6f1250c005a71aa17b3840
Josue <joshvlbn@gmail.com>
2025-05-08 08:59:46 -0400

    avance

--- src/components/molecules/card-business-overview/CardBusinessOverview.tsx
+++ src/components/molecules/card-business-overview/CardBusinessOverview.tsx
@@ -2,10 +2,10 @@ import { IconArrowDownBlack, IconInfopOutlinedPurple } from "@/utils/icons"
 
 const CardBusinessOverview = () => {
     return (
-        <div className="bg-violet-50 flex justify-between px-3 py-4 rounded-xl w-[100%] max-w-[245px]">
+        <div className="bg-violet-50 flex justify-between px-3 py-4 rounded-xl w-[100%] max-w-[250px]">
             <div className="flex">
-                <div className="bg-white rounded-full w-12 h-12 flex items-center justify-center">I</div>
-                <div className="ml-3">
+                <div className="bg-white rounded-full w-11 h-11 flex items-center justify-center">I</div>
+                <div className="ml-2">
                     <div className="flex items-center">
                         <p className="text-xl font-semibold">500</p>
                         <div className="flex items-center ml-2 p-0 bg-white h-3 rounded-xs">
--- src/components/organism/business-overview/BusinessOverview.tsx
+++ src/components/organism/business-overview/BusinessOverview.tsx
@@ -2,7 +2,7 @@ import { CardBusinessOverview } from "@/components/molecules"
 
 const BusinessOverview = () => {
     return (
-        <div className="flex gap-y-3 gap-x-3 my-3 flex-wrap justify-center max-w-[1280px] mx-auto">
+        <div className="flex gap-y-1 gap-x-1 my-3 flex-wrap justify-center max-w-[1300px] mx-auto">
             <CardBusinessOverview />
             <CardBusinessOverview />
             <CardBusinessOverview />
