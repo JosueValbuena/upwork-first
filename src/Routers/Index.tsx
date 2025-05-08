f4835045d62ba05004a808fc43236425619f570a
Josue <joshvlbn@gmail.com>
2025-05-08 11:46:10 -0400

    avance

--- src/Routers/Index.tsx
+++ src/Routers/Index.tsx
@@ -1,9 +1,10 @@
 import Dashboard from "@/pages/dashboard/Index"
 import Settings from "@/pages/settins/Index";
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 const Routers = () => {
     return (
         <Routes>
+            <Route path="/" element={<Navigate to="/dashboard" replace />} />
             <Route index path="/dashboard" element={<Dashboard />} />
             <Route path="/settings" element={<Settings />} />
         </Routes>
