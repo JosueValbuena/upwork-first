513dacc492bc9787ad30942bea7b164d5a3ef9b3
Josue <joshvlbn@gmail.com>
2025-05-11 04:06:07 -0400

    avance

--- src/components/organism/chartsLinear/ChartsLinear.tsx
+++ src/components/organism/chartsLinear/ChartsLinear.tsx
@@ -13,7 +13,7 @@ const ChartsLinear = ({ chartData }: any) => {
     return (
         <ResponsiveContainer
             width='100%'
-            height={300}
+            height={375}
         >
             <LineChart
                 data={chartData}
