8a53c35ee77fabeb23b2d3c0812c14df221e8167
Josue <joshvlbn@gmail.com>
2025-05-29 09:54:25 -0400

    quitado max-w-[1366px] de marketplaces settings

--- src/components/organism/active-marketplaces-section/ActiveMarketplacesSection.tsx
+++ src/components/organism/active-marketplaces-section/ActiveMarketplacesSection.tsx
@@ -4,7 +4,7 @@ import TableCustomTwo from "../table-custom-two/TableCustomTwo"
 const ActiveMarketplacesSection = () => {
     return (
         <div className="p-3 flex flex-col gap-3 bg-primary-light mt-6">
-            <div className="w-full max-w-[1366px] mx-auto flex">
+            <div className="w-full mx-auto flex">
 
                 <div className="grow-1">
                     <TableCustomTwo />
