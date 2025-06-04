42de2b227dd51435affc257e4426b17f43a0f1c5
Josue <joshvlbn@gmail.com>
2025-06-03 21:38:11 -0400

    padding eliminado en segundo box de activemarketplaces

--- src/components/organism/active-marketplaces-section/ActiveMarketplacesSection.tsx
+++ src/components/organism/active-marketplaces-section/ActiveMarketplacesSection.tsx
@@ -56,7 +56,7 @@ const ActiveMarketplacesSection = () => {
 
     return (
         <div className="p-3 flex flex-col gap-3 bg-primary-light mt-6 md:px-10 h-full">
-            <div className="w-full max-w-[1366px] mx-auto flex flex-col-reverse md:p-5 lg:flex-row">
+            <div className="w-full max-w-[1366px] mx-auto flex flex-col-reverse md:py-5 lg:flex-row">
 
                 <div className="grow-1 pt-5">
                     <TableCustomTwo tableHeadTitles={tableHeadTitles} tableContent={tableData} />
